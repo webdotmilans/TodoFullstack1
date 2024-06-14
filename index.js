4c9fd8335a44d3ce30ed2352194eca058b7abaed
webdotmilan <milans708688@gmail.com>
2024-06-14 18:35:29 +0530

    input validation done

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 const  express =require("express");
 const app = express();
-
+const {createTodo, updateTodo} = require("./types");
 app.use(express.json());
 
 //body {
@@ -16,6 +16,15 @@ app.use(express.json());
 
 app.post("todo/", function(res,req){
 
+  const createPayload=req.body;
+  const parsePayload= createTodo.safeParse(createPayload);
+  if(!parsePayload.success){
+    res.statusCode(411).json({
+      msg:"you sent the write inputs",
+    })
+    return;
+  }
+
 })
 
 app.get("/todos", function(res,req){
@@ -23,5 +32,13 @@ app.get("/todos", function(res,req){
 })
 
 app.put("/completed", function(res,req){
+  const updatePayload=req.body;
+  const parsePayload=updateTodo.safeParse(updatePayload);
+  if(!parsePayload.sucees){
+    res.status(411).json({
+      msg:"you sent the write inputs"
+    })
+    return;
+  }
 
 })
